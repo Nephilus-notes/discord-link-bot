c630b87a4852b24c35e7a85b3846bc08f62b5f12
Nephilus-notes <114515979+Nephilus-notes@users.noreply.github.com>
2024-01-22 23:08:54 -0600

    customclient version

--- link_bot.py
+++ link_bot.py
@@ -10,16 +10,28 @@ GUILD = os.getenv('DISCORD_GUILD')
 
 intents = discord.Intents.default()
 
-client = discord.Client(intents=intents)
+# client = discord.Client(intents=intents)
 
-@client.event
-async def on_ready():
-    guild = discord.utils.get(client.guilds, name=GUILD)
+class LinkBotClient(discord.Client):
+    async def on_ready(self):
+        print(f'{self.user} has connected to Discord!')
+
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+
+        if message.content == 'hello':
+            await message.channel.send('Hello!')
+
+
+# @client.event
+# async def on_ready():
+#     guild = discord.utils.get(client.guilds, name=GUILD)
     
-    print(
-        f'{client.user} is connected to the following guild: \n'
-        f'{guild.name} (id: {guild.id})'
-    )
+#     print(
+#         f'{client.user} is connected to the following guild: \n'
+#         f'{guild.name} (id: {guild.id})'
+#     )
     # print(f'{client.user} has connected to Discord!')
-
+client = LinkBotClient(intents=intents)
 client.run(TOKEN)

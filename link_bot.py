0a3f5901779216a71021faee21b5d481891d1a24
Nephilus-notes <114515979+Nephilus-notes@users.noreply.github.com>
2024-01-23 11:56:30 -0600

    discovered bug caused by using list.remove, which is in place and has no return value, for variable assignment

--- link_bot.py
+++ link_bot.py
@@ -24,6 +24,8 @@ async def on_ready():
     
     print(f'{client.user} has connected to the {guild.name} Discord server!')
 
+    await find_new_links(guild)
+
 
 
 @client.event
@@ -50,35 +52,29 @@ async def on_message(message):
 async def find_new_links(guild):
     target_channel = discord.utils.get(guild.channels, name=CHANNEL)
 
-    unchecked_channels = guild.text_channels.remove(target_channel)
-    all_links = []
+    unchecked_channels = [guild for guild in guild.text_channels if guild != target_channel]
+    new_links = []
     links_in_correct_channel = []
 
     async for message in target_channel.history(limit=200):
         if re.search(LINK_PATTERN, message.content):
-            all_links.append(re.search(LINK_PATTERN, message.content).group())
+            links_in_correct_channel.append(re.search(LINK_PATTERN, message.content).group())
+    if links_in_correct_channel:
+        print(f'got all links in {target_channel}')
+
 
     for channel in unchecked_channels:
+        print(f'checking {channel}')
         async for message in channel.history(limit=200):
-            if re.search(LINK_PATTERN, message.content) and re.search(LINK_PATTERN, message.content).group() not in all_links:
-                link = re.search(LINK_PATTERN, message.content).group()
-                https_follow_up = re.search(r'(?<=https:\/\/)[a-zA-Z0-9]{2,}', message.content).group()
-                # grab the first part of the url so the switch can determine where to grab the name from
-        
-                name = ''
-                match https_follow_up:
-                    case 'docs':
-                        print('docs detected')
-                        name = re.search(r'(?<=docs.)[a-zA-Z0-9]{2,}', message.content).group()
-                    case 'www':
-                        print('www detected')
-                        name = re.search(r'(?<=www.)[a-zA-Z0-9]{2,}', message.content).group()
-                    case _:
-                        print('nothing specific detected')
-                        name = re.search(r'(?<=https://)[a-zA-Z0-9]{2,}', message.content).group()
+            if re.search(LINK_PATTERN, message.content) and re.search(LINK_PATTERN, message.content).group() not in links_in_correct_channel:
+                print(f'new link found: {re.search(LINK_PATTERN, message.content).group()}')
+                new_links.append(link_message_builder(message))
 
+    
+    # if new_links:
+    #     print(f'new links found: {new_links}')
+    #     await target_channel.send(new_links)
 
-    pass
 
 def set_target_channel(guild, channel):
     return discord.utils.get(guild.channels, name=channel)
